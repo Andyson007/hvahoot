ded42c5eea6368d8a45d4500cfb9b27f5aded6d5
averieyy <58344917+averieyy@users.noreply.github.com>
2025-03-18 11:16:00 +0100

    Added header and main site content

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,1 +1,19 @@
-<h1>Hvahoot</h1>
+<script>
+  import Centermain from "$lib/components/centermain.svelte";
+import Header from "$lib/components/header.svelte";
+
+</script>
+
+<Header />
+<Centermain>
+  <h1>Hvahoot - Quiz som du aldri har sett det</h1>
+  <p>
+    Hvahoot er et quiz-spill der du kjemper mot andre i en stor kamp om kunnskap. Det er om å gjøre å være den mest kunnskapsrike - og kun én står igjen på toppen til slutt.
+  </p>
+  <div class="center">
+    <a href="/spill" class="button">Spill nå!</a>
+  </div>
+</Centermain>
+
+<style>
+</style>
