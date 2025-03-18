2292802e12c1db16917cd0991a9f0d7b2718df4a
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2025-03-18 11:02:13 +0100

    who wants manual http anyways

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,28 +1,11 @@
-use std::{convert::Infallible, io};
+use rocket::{get, launch, routes};
 
-use backend::http::Header;
-use tokio::{
-    io::AsyncWriteExt,
-    net::{TcpListener, TcpStream},
-};
-
-#[tokio::main]
-async fn main() -> io::Result<Infallible> {
-    let listener = TcpListener::bind("127.0.0.1:8444").await?;
-
-    loop {
-        let (socket, _) = listener.accept().await?;
-        handle_socket(socket).await;
-    }
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
 
-async fn handle_socket(mut stream: TcpStream) {
-    let data = Header::new(&mut stream).await;
-
-    println!("{data:#?}");
-
-    let response = "HTTP/1.1 200 OK\r\n\r\nhello";
-    if data.is_ok() {
-        stream.write_all(response.as_bytes()).await.unwrap();
-    }
+#[get("/")]
+fn index() -> &'static str {
+    "foo"
 }
