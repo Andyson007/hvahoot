35a6cd0112f59dfc4f09ec56041343523c87e3a7
averieyy <58344917+averieyy@users.noreply.github.com>
2025-03-21 09:35:50 +0100

    Recieving and displaying code

--- frontend/src/routes/host/+page.svelte
+++ frontend/src/routes/host/+page.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
-    import { page } from "$app/state";
+  import { page } from "$app/state";
   import { onMount } from "svelte";
 
   let ws: WebSocket;
+  let code: number | undefined = $state(undefined);
 
   onMount(() => {
     const uuid = page.url.searchParams.get('uuid');
     if (!uuid) window.location.href = '/';
 
     ws = new WebSocket('/play/host/'+uuid);
+
+    ws.addEventListener('message', ev => {
+      const { data } = ev.data;
+      const json = JSON.parse(data);
+      
+      switch(json.type) {
+        case 'code':
+          code = json.code;
+          break;
+      }
+    })
   });
 
   function nextQ () {
@@ -23,3 +35,10 @@
 </svelte:head>
 
 <button onclick={nextQ}>Next question</button>
+<span class="code">{code || 'Loading code'}</span>
+
+<style>
+  .code {
+    font-size: 2rem;
+  }
+</style>
